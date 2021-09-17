35ca58523a1be9514388b556d8bd0709faa4f67a
Isaac Munoz <isaacmunoz1490@gmail.com>
2021-09-17 11:36:19 -0700

    Morgan Middleware and Express Set Up

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const morgan = require('morgan');
+const morgan = require('morgan');
 // static pages were added using path and fs modules.
 
 
@@ -7,11 +7,13 @@ const express = require('express');
 const hostname = 'localhost';
 const port = 4000;
 
+
 const app = express();
-// app.use(morgan('dev'));
+app.use(morgan('dev')); //middleware for logging
+
+app.use(express.static(__dirname + '/public')); //absolute path where the files are in
 
 app.use((req, res) => {
-    console.log(req.headers);
     res.statusCode= 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an FF Express Server</h1></body></html>');
@@ -20,4 +22,4 @@ app.use((req, res) => {
 app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}: ${port}/`);
 });
-// Basic Express set up.
+// Basic Express set up, Morgan middleware for logging was installed
